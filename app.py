399be606dfcd3a728c33bb3e5cb01ad4ebc32e15
Pete LePage <pete.lepage@pobox.com>
2014-05-16 19:59:23 -0400

    AppEngine updates

--- app.py
+++ app.py
@@ -1,35 +1,49 @@
 import os
-import json
 import jinja2
 import logging
 import webapp2
-
-
 from google.appengine.api import memcache
+
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
-class MainPage(webapp2.RequestHandler):
-  def get(self):
-    template_values = {}
-    template = JINJA_ENVIRONMENT.get_template("/static/index.html")
-    self.response.write(template.render(template_values))
+class SiteHandler(webapp2.RequestHandler):
+  def find_file(self, request_path):
+    if request_path.endswith("/") == False:
+      request_path += "/"
+    result = memcache.get(request_path)
+    if result is None:
+      template_values = {}
+      try:
+        template = JINJA_ENVIRONMENT.get_template(request_path)
+        result = template.render(template_values)
+      except jinja2.TemplateNotFound:
+        try:
+          url = request_path + "index.html"
+          template = JINJA_ENVIRONMENT.get_template(url)
+          result = template.render(template_values)
+        except jinja2.TemplateNotFound:
+          try:
+            url = request_path[:-1] + ".html"
+            template = JINJA_ENVIRONMENT.get_template(url)
+            result = template.render(template_values)
+          except:
+            result = None
+      memcache.set(request_path, result)
+    return result
 
-class NotFound(webapp2.RequestHandler):
-  def post(self):
-    self.sendResult()
-  def get(self):
-    self.sendResult()
 
-  def sendResult(self):
-    self.response.set_status(404)
-    template_values = {}
-    template = JINJA_ENVIRONMENT.get_template("/static/404.html")
-    self.response.write(template.render(template_values))
+  def get(self):
+    request_path = self.request.path
+    response = self.find_file(request_path)
+    if response is None:
+      self.response.set_status(404)
+      response = self.find_file("/static/404.html")
+    self.response.write(response)
+
 
 
 application = webapp2.WSGIApplication([
-    ('/', MainPage),
-    ('.*', NotFound)
+    ('/.*', SiteHandler)
 ], debug=True)
